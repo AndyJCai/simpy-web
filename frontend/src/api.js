177f8e9279c1ea69322bb5cf6976eb1614d35c66
Andy Cai <andycai20000212@gmail.com>
2020-01-23 02:30:59 -0800

    Added axios functions

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,19 @@
 import axios from 'axios';
+
+const url = "https://localhost:8888";
+
+// GET function to get user's top artists
+export const getUserTopArtists = function() {
+    axios.get(url + '/top/artists').then(res => {
+      const data = res.data;
+      console.log(data);
+    });
+}
+
+// GET function to get user's top tracks
+export const getUserTopTracks = function() {
+    axios.get(url + '/top/tracks').then(res => {
+      const data = res.data;
+      console.log(data);
+    });
+}
