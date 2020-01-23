caa3d545fb6810804aa6a70615fe1827d91a72ad
Andy Cai <andycai20000212@gmail.com>
2020-01-22 23:27:53 -0800

    Renamed simpy_users db

--- backend/mongo/mongohandler.js
+++ backend/mongo/mongohandler.js
@@ -5,7 +5,7 @@ class MongoHandler {
     constructor() {
         this.RECEIPT_DB_URL = "mongodb://localhost:27017/simpy_db";
         this.client = mongoose.createConnection(this.RECEIPT_DB_URL, { useNewUrlParser: true });
-        this.UserMapping = this.client.model('simpy-users', User);
+        this.UserMapping = this.client.model('simpy_users', User);
     }
 
     close() {
@@ -15,15 +15,16 @@ class MongoHandler {
 
     async addNewUser(newUser) {
         const { UserSchema } = require('../schemas/user');
-        const userSchema = mongoose.model('simpy-users', UserSchema);
-        var _newUser = new userSchema({ spotify_id: newUser.id, email: newUser.email, fullName: newUser.display_name });
-        _newUser.save(function (err) {
-            if (err) return handleError(err); // saved!
+        const userSchema = mongoose.model('simpy_users', UserSchema);
+
+        userSchema.create({ spotify_id: newUser.id, email: newUser.email, fullName: newUser.display_name }, function (err, small) {
+            if (err) return handleError(err);
+            // saved!
           });
     }
 
     async queryByIDPromise(id, id_owner) {
-        const simpy_users = this.client.collection('simpy-users');
+        const simpy_users = this.client.collection('simpy_users');
         var asyncUsers = new Promise(function (resolve, reject) {
             simpy_users.find({ id_owner: id }, { _id: 0 }).toArray((err, users_with_matching_ids) => {
                 if (err) throw err;
