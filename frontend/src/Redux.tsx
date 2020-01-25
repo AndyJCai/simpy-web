f97bd38dfe43dcc0d506834caad02d62eb53b0f1
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-01-24 22:25:03 -0800

    Initialize redux file

---
+++ frontend/src/Redux.tsx
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+
+// Actions
+
+// Reducer
+function reducer(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({ reducer });
+export default rootReducer;
+
+// Action functions
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Reducer from "./Redux";
+
+let store = createStore(Reducer);
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route></Route>
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
