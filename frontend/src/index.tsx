2af9b4703ebe729e1a1186420c53d8736b42fc17
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-01-26 16:48:05 -0800

    Implemented basic layout of onboarding

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import Reducer from "./Redux";
+
+import Onboard from "./pages/Onboard";
 
 let store = createStore(Reducer);
 
@@ -12,7 +15,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route></Route>
+        <Route exact path="/" component={Onboard}></Route>
       </Switch>
     </Router>
   </Provider>,
--- frontend/src/pages/Onboard.tsx
+++ frontend/src/pages/Onboard.tsx
@@ -9,13 +9,46 @@ const Wrapper = styled("div")`
   align-items: center;
 `;
 
-const Welcome = styled();
+const Content = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+`;
+
+const Welcome = styled("p")`
+  font-size: 48px;
+  font-weight: bold;
+  margin: 0px auto;
+  width: 70%;
+`;
+
+const Desc = styled("p")`
+  font-size: 24px;
+  width: 70%;
+`;
+
+const SpotifyButton = styled("div")`
+  border: 0.5px solid black;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 24px;
+  margin: 50px auto 0px;
+  padding: 25px 70px;
+`;
 
 export default class Onboard extends React.Component {
   render() {
     return (
       <>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <Content>
+            <Welcome>Welcome to Simpy.</Welcome>
+            <Desc>Discover what your friends are listening to.</Desc>
+            <SpotifyButton>Sign up with Spotify</SpotifyButton>
+          </Content>
+        </Wrapper>
       </>
     );
   }
