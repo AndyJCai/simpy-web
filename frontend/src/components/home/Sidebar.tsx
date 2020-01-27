2e83369a025b4306f05c57baf47c246702410127
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-01-26 18:32:11 -0800

    Completed sidebar w/o responsiveness

--- frontend/src/components/home/Sidebar.tsx
+++ frontend/src/components/home/Sidebar.tsx
@@ -13,7 +13,7 @@ const Wrapper = styled("div")`
   top: 0px;
   left: 0px;
   font-family: Helvetica, sans-serif;
-  font-size: 24px;
+  font-size: 20px;
 `;
 
 const ProfilePic = styled("img")`
@@ -22,20 +22,22 @@ const ProfilePic = styled("img")`
   height: 140px;
   background: grey;
   margin: 60px;
+  margin-bottom: 0px;
 `;
 
 const Prof = styled("div")`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const Settings = styled("div")`
   border-top: black solid 1px;
   cursor: pointer;
   display: flex;
-  font-size: 20px;
   align-items: center;
+  width: 100%;
 `;
 
 const Cog = styled("div")`
@@ -46,6 +48,26 @@ const Cog = styled("div")`
   margin: 10px 12px 10px 40px;
 `;
 
+const Name = styled("p")`
+  font-size: 32px;
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 0px;
+`;
+
+const Handle = styled("p")`
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const Friends = styled("div")`
+  width: 70%;
+  text-align: center;
+  border: 0.5px black solid;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
 export default class Sidebar extends React.Component {
   render() {
     return (
@@ -53,6 +75,9 @@ export default class Sidebar extends React.Component {
         <Wrapper>
           <Prof>
             <ProfilePic src="" />
+            <Name>Henry</Name>
+            <Handle>@username</Handle>
+            <Friends>100 Friends</Friends>
           </Prof>
           <Settings>
             <Cog />
