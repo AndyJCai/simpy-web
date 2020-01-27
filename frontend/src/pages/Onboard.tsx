dea3d18b98ee3840890fbeaf4f14cb5e1a211cbc
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-01-26 16:32:18 -0800

    Started onboard page

---
+++ frontend/src/pages/Onboard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled("div")`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Welcome = styled();
+
+export default class Onboard extends React.Component {
+  render() {
+    return (
+      <>
+        <Wrapper></Wrapper>
+      </>
+    );
+  }
+}
