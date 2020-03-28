080415be43bd9785ba9ee783b2ad0d4e134c9f00
Andy Cai <andycai20000212@gmail.com>
2020-03-27 21:19:56 -0700

    Finished initial PostSchema

--- backend/schemas/post.js
+++ backend/schemas/post.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// check this stackoverflow forum out:
+// https://stackoverflow.com/questions/1443960/how-to-implement-the-activity-stream-in-a-social-network
+
 const PostSchema = new Schema(
 	{
-		creator: { type: Schema.Types.ObjectId, ref: 'User' },
-		body: { type: String, required: true },
-		created_at: { type: Date, default: Date.now },
+		user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true }, //user, could be the sender
+		receiver_id: { type: Schema.Types.ObjectId, ref: 'User' },
+		activity_type: {
+			type: Number,
+			required: true,
+			default: 0,
+			/*
+			1 - vibe check songs aka. compare top tracks between users: null
+			2 - vibe check artists - source: null
+			3 - system recommend song - source: track_id 
+			4 - user recommend (send) song - source: track_id
+		*/
+		},
+		source_id: { type: String }, // the record that the activity is related to, refer to above
+		created_time: { type: Date, default: Date.now, required: true },
 	},
 	{
 		collection: 'Post',
+		timestamps: { type: Date, default: Date.now },
 	}
 );
 
