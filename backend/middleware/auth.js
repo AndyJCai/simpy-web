c92976385235b2053cd9e0e62da7d273afaeaeb3
Rahul Natarajan <rahulnatarajan@Rahuls-MacBook-Pro-1453.local>
2020-03-28 12:50:30 -0700

    fixed up user parser

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ class Middleware {
     verify = (app) => {
         app.use('/auth', function(req, res, next) {
             if(!req.headers['access']){
-                res.status(403);
+                res.status(401);
                 return;
             }
             next();
@@ -12,8 +12,13 @@ class Middleware {
     get_current_user = (req) => { //FIXME: this function doesn't actually work?
         if (req == null)
             return null;
-        console.log(req.headers['access']);
-        return req.headers['access'];
+        return req.headers['access'].split("Bearer ")[1];
+    }
+
+    get_auth_token = (req) => {
+        if (req.cookies['access_token'] == null)
+            return;
+        return req.cookies['access_token'];
     }
 
 }
