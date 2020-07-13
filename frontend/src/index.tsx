d786cb458217ce110e7858e9f4c3f78c0c534821
Andy Cai <andycai20000212@gmail.com>
2020-07-12 23:55:42 -0700

    Added local storage for persistent redux states

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,11 +5,20 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import reduxThunk from "redux-thunk";
-
+import { loadState, saveState } from './reducers/localStorage';
 import App from "./App";
+import throttle from 'lodash/throttle';
+
+
+const peristedState = loadState();
 
 const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+const store = createStore(reducers, peristedState,  composeEnhancers(applyMiddleware(reduxThunk)));
+
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 1000));
+
 
 ReactDOM.render(
   <Provider store={store}>
